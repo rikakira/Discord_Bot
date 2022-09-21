bdfa93e299f1ce98acdc8e2de455089b201b4cff
Ritika <73664570+rikakira@users.noreply.github.com>
2022-09-21 18:24:11 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import discord
+import random
+
+intents = discord.Intents.default()
+token = "MTAyMjAzMzQ5OTcxNDIyODI5NA.GNr5Fa.m-6z2fCdWB9M-LDIQI0ZDEqbT_AZx_lpnWQtoY"
+
+client = discord.Client(intents=intents)
+
+
+@client.event
+async def on_ready():
+    print(f"Bot logged in as {client.user}")
+
+
+@client.event
+async def on_message(message):
+    username = str(message.author).split('#')[0]
+    user_message = str(message.content)
+    channel = str(message.channel.name)
+    print(f'{username}:{user_message} ({channel})')
+
+    if message.author == client.user:
+        return
+    if message.channel.name == 'bot-channel':
+        if user_message.lower() == 'hello':
+            await message.channel.send(f'Hello {username}!')
+            return
+        elif user_message.lower() == 'bye':
+            await message.channel.send(f'See you later {username}!')
+            return
+        elif user_message.lower() == '!random':
+            response = f'This is your random number: {random.randrange(1000000)}'
+            await message.channel.send(response)
+            return
+    if user_message.lower() == '!anywhere':
+        await message.channel.send('This can be used anywhere!')
+        return
+
+
+client.run(token)
